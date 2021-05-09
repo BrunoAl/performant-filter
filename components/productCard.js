fda6c168b5c24d6b8e65b61b5cb026d80675bbaf
Bruno <brunoalnascimento@gmail.com>
2021-05-09 23:16:47 +0200

    optimize images

--- components/productCard.js
+++ components/productCard.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Image from 'next/image';
 import styles from './productCard.module.css';
 
 export default function ProductCard({ data }) {
   return (
     <div className={styles.card}>
-      <img className={styles.card__img} src={data.thumbnailImage.file.url} alt={data.name} />
+      <Image
+        className={styles.card__img}
+        src={`http:${data.thumbnailImage.file.url}`}
+        alt={data.name}
+        width={400}
+        height={400}
+      />
       <div className={styles.card__info}>
         <h3 className={styles.card__title}>{data.name}</h3>
         <span className={styles.card__price}>€{data.shopifyProductEu.variants.edges[0].node.price}</span>
